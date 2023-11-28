01302dc6c5435169172e98517eea6845e69596d2
Amol Pounikar <amolpounikar100@gmail.com>
2023-11-28 22:33:57 +0530

    change in server

--- server/dataBase/conn.js
+++ server/dataBase/conn.js
@@ -1,14 +1,18 @@
 import mongoose from "mongoose";
 import { MongoMemoryServer } from 'mongodb-memory-server'
 
+// Load environment variables from .env file
+import dotenv from 'dotenv';
+dotenv.config();
+
 async function connect() {
     const mongod = await MongoMemoryServer.create()
     const getUri = mongod.getUri()
 
     mongoose.set('strictQuery', true)
-    const db = await mongoose.connect(getUri)
+    const db = await mongoose.connect(process.env.ATLAS_URI)
     console.log('Database is connected')
     return db
 }
 
-export default connect
+export default connect;
